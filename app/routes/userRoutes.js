c89140b58157a3c4b167f63424260f0128cffab7
Shubham <shubham@Shubhams-MacBook-Pro.local>
2019-04-29 22:27:20 +0530

    new commit

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -2,6 +2,4 @@ const user = require('../controllers/userControllers')
 module.exports = (app) => {
     app.post('/user/addItem',user.addItem);
     app.get('/user/getItem',user.getItem);
-    app.post('/user/login', user.login);
-    app.post('/user/register', user.register);
 }
